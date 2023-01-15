0e5f196b24d28157a9ded4346dae98fc90555fc8
kcharshini <krishnachaitanya.kota4@gmail.com>
2023-01-15 15:11:19 -0600

    Initial commit for GithubActions Demo

---
+++ src/main/java/com/githubActions/Controller/GitHubActionsDemo.java
@@ -0,0 +1,14 @@
+package com.githubActions.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitHubActionsDemo {
+	
+	@GetMapping("/hello")
+	public String getMessage() {
+		return "Hello KC!";
+	}
+
+}
