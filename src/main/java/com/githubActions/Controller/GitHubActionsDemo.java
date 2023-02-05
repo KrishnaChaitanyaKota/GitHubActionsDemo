ce749274b4143c2ab6df48cc9bb1472694427cca
kcharshini <krishnachaitanya.kota4@gmail.com>
2023-02-05 13:49:26 -0600

    Accessing Secrets in application.properties

--- src/main/java/com/githubActions/Controller/GitHubActionsDemo.java
+++ src/main/java/com/githubActions/Controller/GitHubActionsDemo.java
@@ -1,13 +1,18 @@
 package com.githubActions.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitHubActionsDemo {
 	
+	@Value("${sever.port}")
+	private int port;
+	
 	@GetMapping("/hello")
 	public String getMessage() {
+		System.out.println(port);
 		return "Hello KC! $$";
 	}
 
